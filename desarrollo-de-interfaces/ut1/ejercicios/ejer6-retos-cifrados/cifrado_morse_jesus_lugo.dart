d213478107190632d3a698856d41ab433cd70600
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-10-01 15:11:21 +0100

    Finish exercise 6 of DAD: decoding challenges

--- desarrollo-de-interfaces/ut1/ejercicios/ejer6-retos-cifrados/cifrado_morse_jesus_lugo.dart
+++ desarrollo-de-interfaces/ut1/ejercicios/ejer6-retos-cifrados/cifrado_morse_jesus_lugo.dart
@@ -1,6 +1,5 @@
 void main() {
-  String decodeMorse(String morse) {
-    final Map<String, String> morseCodeMap = {
+  final Map<String, String> morseCodeMap = {
     'A': '.-',
     'B': '-...',
     'C': '-.-.',
@@ -41,18 +40,32 @@ void main() {
     ',': '--..--',
     '?': '..--..',
     '/': '-..-.',
-    ' ': '/'
-    };
+    ' ': '/',
+  };
 
-    List<String> words = morse.split("/");
-    for (var word in words) {
-      List<String> letters = word.split(" ");
-      letters.map((l) => {
-        
-      });
+  String decodeWord(String word) {
+    List<String> decodedLetters = [];
+    List<String> morseLetters = word.split(" ");
+
+    for (var letter in morseLetters) {
+      String decodedLetter = morseCodeMap.keys.firstWhere(
+        (k) => morseCodeMap[k] == letter,
+      );
+      decodedLetters.add(decodedLetter);
     }
+    return decodedLetters.join();
+  }
+
+  String decodeMorse(String morse) {
+    List<String> words = morse.split("/");
+    List<String> decodedWords = [];
+    for (var word in words) {
+      decodedWords.add(decodeWord(word.trim()));
+    }
+    return decodedWords.join(" ");
   }
 
   String secretMessage =
       "- --- -.-. / - --- -.-. / - --- -.-. --..-- / .--. . -. -. -.--";
-  print(decodeMorse(secretMessage));
+  print("Resultado: " + decodeMorse(secretMessage));
+}
