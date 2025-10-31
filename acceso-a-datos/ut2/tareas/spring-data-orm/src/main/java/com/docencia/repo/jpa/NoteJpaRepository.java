afd40e236f4d56b082cfa5c6b55be95214d2d8f2
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-10-31 15:37:17 +0000

    Finish NoteJpaRepository.java

--- acceso-a-datos/ut2/tareas/spring-data-orm/src/main/java/com/docencia/repo/jpa/NoteJpaRepository.java
+++ acceso-a-datos/ut2/tareas/spring-data-orm/src/main/java/com/docencia/repo/jpa/NoteJpaRepository.java
@@ -1,11 +1,14 @@
 package com.docencia.repo.jpa;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Repository;
 
 import com.docencia.model.Note;
 import com.docencia.repo.INoteRepository;
+
+import io.micrometer.common.util.StringUtils;
 
 @Repository
 public class NoteJpaRepository implements INoteRepository {
@@ -34,32 +37,34 @@ public class NoteJpaRepository implements INoteRepository {
 
     @Override
     public Note findById(String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        return repository.findById(id).orElse(null);
     }
 
     @Override
     public Note find(Note note) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'find'");
+        return this.findById(note.getId());
     }
 
     @Override
     public List<Note> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        return repository.findAll();
     }
 
     @Override
     public Note save(Note note) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+        if (note.getId() == null || StringUtils.isEmpty(note.getId())) {
+            note.setId(UUID.randomUUID().toString());
+        }
+        return repository.save(note);
     }
 
     @Override
     public boolean delete(String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        if (!exists(id)) {
+            return false;
+        }
+        repository.deleteById(id);
+        return true;
     }
 
 }
