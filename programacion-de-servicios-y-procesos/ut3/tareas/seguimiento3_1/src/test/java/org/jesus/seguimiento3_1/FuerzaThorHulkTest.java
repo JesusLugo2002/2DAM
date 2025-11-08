f5fe604e616b63326052e66d3b1062d2a20f8d5c
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-11-08 20:03:37 +0000

    Finish exercise 8

---
+++ programacion-de-servicios-y-procesos/ut3/tareas/seguimiento3_1/src/test/java/org/jesus/seguimiento3_1/FuerzaThorHulkTest.java
@@ -0,0 +1,24 @@
+package org.jesus.seguimiento3_1;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.jesus.seguimiento3_1.classes.FuerzaThorHulk;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FuerzaThorHulkTest {
+    
+    @Test
+    public void testFuerzaThorHulk() throws InterruptedException {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        FuerzaThorHulk fuerza = new FuerzaThorHulk();
+        fuerza.main(null);
+        String output = outContent.toString();
+
+        Assertions.assertTrue(output.contains("Tiempo"));
+        Assertions.assertTrue(output.contains("gana") || output.contains("Empate"));
+    }
+}
