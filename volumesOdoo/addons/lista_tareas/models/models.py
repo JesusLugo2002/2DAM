60b93a0a5a5e4bc43b4b708b2949b222b7ac37f2
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-12-04 15:56:45 +0000

    feat: add form view in lista_tareas

--- volumesOdoo/addons/lista_tareas/models/models.py
+++ volumesOdoo/addons/lista_tareas/models/models.py
@@ -16,7 +16,7 @@ class ListaTareas(models.Model):
     realizada = fields.Boolean(string='Realizada')
     # El campo asignado_a lo volví opcional porque pueden existir tareas que no necesariamente
     # estén asignadas para una persona, pues puede no tener importancia quién haga la tarea.
-    asignado_a = fields.Many2one('res.users', string='asignado_a', required=False, default=lambda self: self.env.user)
+    asignado_a = fields.Many2one('res.users', string='Asignado a', required=False, default=lambda self: self.env.user)
 
     @api.depends('prioridad')
     def _value_urgente(self):
