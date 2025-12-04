9c4cc1f4befdb48a1fdeee7b8e533e7f69ffc28d
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-12-04 15:25:08 +0000

    feat: add 'asignado_a' field into model

--- volumesOdoo/addons/lista_tareas/models/models.py
+++ volumesOdoo/addons/lista_tareas/models/models.py
@@ -14,6 +14,9 @@ class ListaTareas(models.Model):
         store=True
     )
     realizada = fields.Boolean(string='Realizada')
+    # El campo asignado_a lo volví opcional porque pueden existir tareas que no necesariamente
+    # estén asignadas para una persona, pues puede no tener importancia quién haga la tarea.
+    asignado_a = fields.Many2one('res.users', string='asignado_a', required=False, default=lambda self: self.env.user)
 
     @api.depends('prioridad')
     def _value_urgente(self):
