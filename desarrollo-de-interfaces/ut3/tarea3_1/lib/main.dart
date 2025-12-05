0abe73dc62d494135310a4847e2ebb9a96b8fd4d
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-12-05 19:56:24 +0000

    feat: finish exercise 2

---
+++ desarrollo-de-interfaces/ut3/tarea3_1/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FadeDemo());
+}
+
+class FadeDemo extends StatefulWidget {
+  const FadeDemo({super.key});
+
+  @override
+  State<FadeDemo> createState() => _FadeDemoState();
+}
+
+class _FadeDemoState extends State<FadeDemo> {
+}
