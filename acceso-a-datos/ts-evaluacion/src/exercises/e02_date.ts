72063e1545e26cf78602e68cf4b1517f443673e1
JesusLugo2002 <jesusdlugo02@gmail.com>
2026-01-14 22:08:43 +0000

    feat: finish e02 exercises

--- acceso-a-datos/ts-evaluacion/src/exercises/e02_date.ts
+++ acceso-a-datos/ts-evaluacion/src/exercises/e02_date.ts
@@ -3,16 +3,33 @@
  */
 
 export function isValidISODate(iso: string): boolean {
-  // regex yyyy-mm-dd + Date válida + conserva componentes (evita 2026-02-30)
-  throw new Error("TODO");
+  const dateISORegex = new RegExp(/^\d{4}(?:\-\d{2}){2}$/);
+  if (iso.match(dateISORegex) == null) {
+    return false;
+  }
+  return iso === toIsoDateOnly(new Date(iso));
 }
 
 export function nightsBetween(entrada: string, salida: string): number {
-  // intervalo [entrada, salida) => noches. Error si salida<=entrada o fechas inválidas
-  throw new Error("TODO");
+  if (!isValidISODate(entrada) || !isValidISODate(salida)) {
+    throw new Error("Invalid dates");
+  }
+  const startTime = new Date(entrada).getTime();
+  const endTime = new Date(salida).getTime();
+  if (endTime <= startTime) {
+    throw new Error("The end time is lower or equals to the starting time")
+  }
+  const timeInMs = endTime - startTime;
+  const msInDay = 1000 * 60 * 60 * 24;
+  return timeInMs / msInDay;
 }
 
 export function toIsoDateOnly(date: Date): string {
-  // "YYYY-MM-DD" desde Date (UTC). Error si date inválida.
-  throw new Error("TODO");
+  let result;
+  try {
+    result = date.toISOString();
+  } catch (e: unknown) {
+    throw new Error("Invalid date");
+  }
+  return result.substring(0, 10);
 }
