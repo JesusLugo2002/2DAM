0ed51bbbdadcc758089a87e3d05d62638968127e
JesusLugo2002 <jesusdlugo02@gmail.com>
2026-01-14 17:08:10 +0000

    feat: adding ts-evaluation

---
+++ acceso-a-datos/ts-evaluacion/src/exercises/e01_basicos.ts
@@ -0,0 +1,23 @@
+/**
+ * E01 – Tipos básicos: string/number/boolean/null/undefined
+ */
+
+export function normalizeBearer(authHeader: string): string {
+  const splittedHeader = authHeader.trim().split(" ");
+  if (!splittedHeader[0].toLowerCase().includes("bearer")) {
+    throw new Error("Invalid header")
+  }
+  const normalizedHeader = splittedHeader.filter((text) => text.length > 0).join(" ");
+  return "B" + normalizedHeader.substring(1);
+}
+
+export function clamp01(value: number): number {
+  if (Number.isNaN(value) || !Number.isFinite(value)) {
+    throw new Error("Valor inválido")
+  }
+  return Math.max(Math.min(value, 1), 0);
+}
+
+export function safeBool(value: boolean | null | undefined): boolean {
+  return value == null || value == undefined ? false : value;
+}
